16b31f02874b9d0869abcbfea1625fd9943e33fb
Rohan George D <rohan.geo.2001@gmail.com>
2020-08-03 00:26:55 +0530

    Updates

---
+++ precache-manifest.4f96d2f602d2ae7e514b30ff51d83e9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8979ea74fb339f5da8963f6a268300",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "c47f9172a76617e943c6",
+    "url": "/React-Calculator/static/css/main.c528b827.chunk.css"
+  },
+  {
+    "revision": "aa7ae3c9dbeb345eab89",
+    "url": "/React-Calculator/static/js/2.679359f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.679359f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c47f9172a76617e943c6",
+    "url": "/React-Calculator/static/js/main.6a98df15.chunk.js"
+  },
+  {
+    "revision": "0d965dd05dbd5b2574b2",
+    "url": "/React-Calculator/static/js/runtime-main.07fcce11.js"
+  }
+]);
